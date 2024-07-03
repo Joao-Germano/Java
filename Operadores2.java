0ee43931ebf56c791e812fdb0728e73a2e7d89ec
Joao-Germano <jaogermano076@gmail.com>
2024-07-03 16:55:16 -0300

    Add files via upload

---
+++ Operadores2.java
@@ -0,0 +1,34 @@
+public class Operadores { // declaração de operadores
+    public static void main(String[] args) throws Exception { // declaração de metodo main
+        System.out.println("Operador de atribuição: ="); // declaração de operador de atribuição 
+        String texto = "Texto"; // declaração de nome variavel
+        System.out.println("String texto = \"Texto\";"); // declaração de parametro 
+        System.out.println(texto); // declaração de nome da variavel texto 
+
+        System.out.println("Operador de soma: +"); // declaração de operador de soma 
+        int soma = 1 + 1; // declaração de nome variavel e calculo 
+        System.out.println("int soma = 1 + 1;"); // declaração da string
+        System.out.println(soma); // declaração de nome variavel soma 
+
+        System.out.println("Operador de subtração: -"); // declaração de operador de subtração 
+        int subtracao = 1 - 1; // declaração de variavel subtração e calculo matematico 
+        System.out.println("int subtracao = 1 - 1;"); // declaração de string subtração
+        System.out.println(subtracao); // declaração de nome da variavel 
+
+        System.out.println("Operador de divisão: /"); // declaração de da string operador de divisão 
+        int divisao = 1 / 1; // declaração de tipo, nome variavel, operador de atribuição, valor, operador matematico, e calculo 
+        System.out.println("int divisao = 1 / 1;"); // declaração de string divisão 
+        System.out.println(divisao); // declaração de nome da variavel divisão
+
+        System.out.println("Operador de multiplicação: *"); // declaração de string operador de mutiplicação 
+        int multiplicacao = 1 * 1; // declaração de tipo, nome variavel, operador de atribuição, valor, operador matematico, e calculo 
+        System.out.println("int multiplicacao = 1 * 1;"); // declaração de string mutiplicação e calculo 
+        System.out.println(multiplicacao); // declaração de nome variavel mutiplicação 
+
+        System.out.println("Operador de concatenação de strings: +"); // declaração de string operador de concatenação de string
+        String concatenacao = "Texto 1 + " + "Texto 2"; // declaração de nome da variavel, operador de atribuição e o + que e concatenador do Texto 1 e Texto 2
+        System.out.println("String concatenacao = \"Texto 1 + \" + \"Texto 2\";"); // declaração de string dos parametros "String concatenacao = \"Texto 1 + \" + \"Texto 2\";"
+        System.out.println(concatenacao); // declaração de nome variavel 
+    } // declaração de metodo principal main
+} // declaração de fechamento do bloco de código 
+
