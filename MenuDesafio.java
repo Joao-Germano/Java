9dc057b0936afbf91b30503f82e74baa6880afa9
Joao-Germano <jaogermano076@gmail.com>
2024-08-15 14:50:15 -0300

    Add files via upload

---
+++ MenuDesafio.java
@@ -0,0 +1,296 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*; 
+
+public class MenuDesafio extends JFrame {
+ private final JTextField idJTextField = new JTextField();
+ private final JTextField nomeJTextField = new JTextField();
+ private final JTextField emailJTextField = new JTextField();
+ private final JTextField senhaJTextField = new JTextField();
+ private final JButton updateJButton = new JButton(""); 
+ private final JButton nextJButton = new JButton(">");
+ private final JButton previousJButton = new JButton("<");
+ private final JButton firstJButton = new JButton("<<");
+ private final JButton lastJButton = new JButton(">>");
+ private final JLabel idJLabel = new JLabel("Id:");
+ private final JLabel nomeJLabel = new JLabel("Nome:");
+ private final JLabel emailJLabel = new JLabel("Email:");
+ private final JLabel senhaJLabel = new JLabel("Senha:");
+ private final JLabel notificacaoJLabel = new JLabel("Notificações...");
+ //private final JLabel espacadorLabel = new JLabel("");
+ private final JLabel espacador2Label = new JLabel("");
+ //private final JLabel espacador3Label = new JLabel("");
+ private final JLabel espacador4Label = new JLabel("");
+ //private final JLabel espacador5Label = new JLabel("");
+ private final JLabel espacador6Label = new JLabel("");
+ //private final JLabel espacador7Label = new JLabel("");
+ private final JButton deleteJButton = new JButton("Deletar");
+ private final JButton novoJButton = new JButton("+");
+ private final JButton verJButton = new JButton("");
+ private final JButton excluirJButton = new JButton("🗑️");
+
+ public MenuDesafio()
+ {
+ super("Cadastrar");
+ setLayout(new GridLayout(6, 4, 10, 10));
+
+ firstJButton.addActionListener(
+    new ActionListener() {
+        public  void actionPerformed(ActionEvent event) {
+            String[] resultado;
+            try {
+                resultado = NavegadorDeRegistro.primeiroRegistro("db_texte","tbl_texte");
+                notificacaoJLabel.setText("Primeiro registro posicionado com sucesso");
+                idJTextField.setText(resultado [0]);
+                nomeJTextField.setText(resultado [1]);
+                emailJTextField.setText(resultado [2]);
+                senhaJTextField.setText(resultado [3]);
+                updateJButton.setEnabled(true);
+                firstJButton.setEnabled(false);
+                previousJButton.setEnabled(false);
+                lastJButton.setEnabled(true);
+                nextJButton.setEnabled(true);
+                deleteJButton.setEnabled(true);
+               // novoJButton.setEnabled(false);
+                //verJButton.setEnabled(false);
+                //editarJButton.setEnabled(false);
+
+
+           }catch(Exception e) {
+             System.out.println("Ops! Ocorreu um erro ao posicionar o registro para o primeiro. Veja o erro"+ e);
+             return;
+           }
+            
+        };
+    }
+ );
+
+ lastJButton.addActionListener(
+    new ActionListener() {
+        public  void actionPerformed(ActionEvent event) {
+            String[] resultado;
+            try {
+                resultado = NavegadorDeRegistro.ultimoRegistro("db_texte","tbl_texte");
+                notificacaoJLabel.setText("Ultimo registro posicionado com sucesso");
+                idJTextField.setText(resultado [0]);
+                nomeJTextField.setText(resultado [1]);
+                emailJTextField.setText(resultado [2]);
+                senhaJTextField.setText(resultado [3]);
+                updateJButton.setEnabled(true);
+                lastJButton.setEnabled(false);
+                nextJButton.setEnabled(false);
+                firstJButton.setEnabled(true);
+                previousJButton.setEnabled(true);
+                deleteJButton.setEnabled(true);
+                //novoJButton.setEnabled(false);
+                //verJButton.setEnabled(false);
+                //editarJButton.setEnabled(false);
+
+           }catch(Exception e) {
+             System.out.println("Ops! Ocorreu um erro ao posicionar o registro para o ultimo. Veja o erro"+ e);
+             return;
+           }
+            
+        };
+    }
+ );
+
+ nextJButton.addActionListener(
+    new ActionListener() {
+        public  void actionPerformed(ActionEvent event) {
+            String[] resultado;
+            try {
+                resultado = NavegadorDeRegistro.NextRegister("db_texte","tbl_texte", idJTextField.getText());
+                notificacaoJLabel.setText("Proximo registro posicionado com sucesso");
+                idJTextField.setText(resultado [0]);
+                nomeJTextField.setText(resultado [1]);
+                emailJTextField.setText(resultado [2]);
+                senhaJTextField.setText(resultado [3]);
+                updateJButton.setEnabled(true);
+                previousJButton.setEnabled(true);
+
+           }catch(Exception e) {
+             System.out.println("Ops! Ocorreu um erro ao posicionar o registro para o proximo. Veja o erro"+ e);
+             return;
+           }
+            
+        };
+    }
+ );
+
+ deleteJButton.addActionListener(
+  new ActionListener() {
+      public void actionPerformed(ActionEvent event)
+{
+String nome;
+String senha;
+String email;
+try
+{
+  nome = nomeJTextField.getText();
+  email = emailJTextField.getText();
+  senha = senhaJTextField.getText();
+  NavegadorDeRegistro.DeleteRegister("db_texte", "tbl_texte", "nome", "email", "senha", nome, email, senha);
+  notificacaoJLabel.setText("Deletado com sucesso");
+  
+} 
+catch(Exception e){
+  System.out.println("Digite alguma coisa.");
+  return;
+   }     
+  }
+ }
+);
+
+updateJButton.addActionListener(
+  new ActionListener() {
+      public void actionPerformed(ActionEvent event)
+{
+String nome;
+String email;
+String senha;
+String id;
+try
+{
+  nome = nomeJTextField.getText();
+  email = emailJTextField.getText();
+  senha = senhaJTextField.getText();
+  id = idJTextField.getText();
+  NavegadorDeRegistro.updateRegistro("db_texte", "tbl_texte",id, nome, email, senha );
+  notificacaoJLabel.setText("Atualização realizada com sucesso");
+} 
+catch(Exception e){
+  System.out.println("Digite alguma coisa.");
+  return;
+   }     
+  }
+ }
+);
+
+novoJButton.addActionListener(
+  new ActionListener() {
+      public void actionPerformed(ActionEvent event)
+{
+String nome;
+String senha;
+String email;
+try
+{
+  nome = nomeJTextField.getText();
+  email = emailJTextField.getText();
+  senha = senhaJTextField.getText();
+  NavegadorDeRegistro.novoRegistro("db_texte", "tbl_texte","nome", "email", "senha", nome, email, senha);
+  notificacaoJLabel.setText("Novo registro adicionado com sucesso");
+} 
+catch(Exception e){
+  System.out.println("Digite alguma coisa.");
+  return;
+   }     
+  }
+ }
+);
+
+verJButton.addActionListener(
+  new ActionListener() {
+      public void actionPerformed(ActionEvent event)
+{
+String nome;
+String senha;
+String email;
+try
+{
+  nome =  nomeJTextField.getText();
+  email = emailJTextField.getText();
+  senha = senhaJTextField.getText();
+NavegadorDeRegistro.verRegistro("db_texte", "tbl_texte","nome", "email", "senha", nome, email, senha);
+  notificacaoJLabel.setText("Pesquisa realizada com sucesso");
+} 
+catch(Exception e){
+  System.out.println("Digite alguma coisa.");
+  return;
+   }     
+  }
+ }
+);
+
+excluirJButton.addActionListener(
+  new ActionListener() {
+      public void actionPerformed(ActionEvent event)
+{
+  nomeJTextField.setText("");
+  emailJTextField.setText("");
+  senhaJTextField.setText("");
+  }
+    }
+      );
+
+ previousJButton.addActionListener(
+    new ActionListener() {
+        public  void actionPerformed(ActionEvent event) {
+            String[] resultado;
+            try {
+                resultado = NavegadorDeRegistro.anteriorRegistro("db_texte","tbl_texte", idJTextField.getText());
+                notificacaoJLabel.setText("Anterior registro posicionado com sucesso");
+                idJTextField.setText(resultado [0]);
+                nomeJTextField.setText(resultado [1]);
+                emailJTextField.setText(resultado [2]);
+                senhaJTextField.setText(resultado [3]);
+                updateJButton.setEnabled(true);
+                nextJButton.setEnabled(true);
+           }catch(Exception e) {
+             System.out.println("Ops! Ocorreu um erro ao posicionar o registro para o anterior. Veja o erro"+ e);
+             return;
+           }
+            
+        };
+    }
+ );
+
+ updateJButton.setEnabled(false);  
+
+ add(idJLabel);
+ add(idJTextField);
+ add(updateJButton);
+ add(deleteJButton);
+// add(espacadorLabel);
+ idJTextField.setEditable(false);
+
+ add(nomeJLabel);
+ add(nomeJTextField);
+ add(espacador2Label);
+ add(novoJButton);
+ //add(espacador3Label);
+
+ add(emailJLabel);
+ add(emailJTextField);
+ add(espacador4Label);
+ add(verJButton);
+ //add(espacador5Label);
+
+ add(senhaJLabel);
+ add(senhaJTextField);
+ add(espacador6Label);
+ add(excluirJButton);
+
+ add(firstJButton);
+ //add(espacador3Label);
+ add(previousJButton);
+ //add(espacador5Label);
+ add(nextJButton);
+ add(lastJButton);
+ 
+ //add(espacador3Label);
+ //add(espacador5Label);
+ add(notificacaoJLabel);
+ 
+ //add(espacador7Label);
+ 
+ setSize(600, 300);
+ setVisible(true);
+
+ }
+ public static void main(String[] args) {
+ MenuDesafio application = new MenuDesafio();
+ application.setDefaultCloseOperation(EXIT_ON_CLOSE);
+ }
+}
---
+++ NavegadorDeRegistro.java
@@ -0,0 +1,200 @@
+ import java.sql.*;
+
+public class NavegadorDeRegistro {
+    public static String[] primeiroRegistro(String db,String tbl) throws Exception {
+        Connection conexao = MySQLConnector.conectar();
+        try{
+        String strSqlPrimeiroRegistro = "select * from `" + db + "`.`" + tbl + "`order by `id` asc limit 1;";
+        Statement stmSqlPrimeiroRegistro = conexao.createStatement();
+        ResultSet rstSqlprimeiroRegistro = stmSqlPrimeiroRegistro.executeQuery(strSqlPrimeiroRegistro);
+        rstSqlprimeiroRegistro.next();
+        String[] resultado = {
+            rstSqlprimeiroRegistro.getString("id"),
+            rstSqlprimeiroRegistro.getString("nome"),
+            rstSqlprimeiroRegistro.getString("email"),
+            rstSqlprimeiroRegistro.getString("senha")
+        };
+            stmSqlPrimeiroRegistro.close();
+            return resultado;
+        } catch(Exception e) {
+            return null;
+        }
+    }
+
+    public static String[] ultimoRegistro(String db, String tbl ) throws Exception {
+        Connection conexao = MySQLConnector.conectar();
+        try{
+        String strSqlUltimoRegistro = "select * from `" + db + "`.`" + tbl + "`order by `id` desc limit 1;";
+        Statement stmSqlUltimoRegistro = conexao.createStatement();
+        ResultSet rstSqlultimoRegistro = stmSqlUltimoRegistro.executeQuery(strSqlUltimoRegistro);
+        rstSqlultimoRegistro.next();
+        String[] resultado = {
+            rstSqlultimoRegistro.getString("id"),
+            rstSqlultimoRegistro.getString("nome"),
+            rstSqlultimoRegistro.getString("email"),
+            rstSqlultimoRegistro.getString("senha")
+        };
+        stmSqlUltimoRegistro.close();
+        return resultado;
+    }catch(Exception e) {
+        return null;
+    }
+
+    } 
+
+    public static String[] NextRegister(String db, String tbl, String id) throws Exception {
+        Connection conexao = MySQLConnector.conectar();
+        int idPerson = Integer.parseInt(id);
+        int nextID = idPerson + 1;
+        if (nextID >= 1) {
+                String strSqlLastRegister = "select * from `" + db + "`.`" + tbl + "` order by `id` asc;";
+                Statement stmNextRegister = conexao.createStatement();
+                ResultSet rstSqlNextRegister = stmNextRegister.executeQuery(strSqlLastRegister);
+                String[] result = {"", "", "", ""};
+
+                while (rstSqlNextRegister.next()) {
+                    if (id.equals(rstSqlNextRegister.getString("id"))) {
+                        rstSqlNextRegister.next();
+                        result[0] = rstSqlNextRegister.getString("id");
+                        result[1] = rstSqlNextRegister.getString("nome");
+                        result[2] = rstSqlNextRegister.getString("email");
+                        result[3] = rstSqlNextRegister.getString("senha");
+                        break;
+
+                    }
+                }
+                rstSqlNextRegister.close();
+                if (result[0] == "") {
+                    return null;
+                } else {
+                    return result;
+                }
+        } else {
+            return null;
+        }
+    }
+
+    public static String[] DeleteRegister(String db, String tbl, String campo, String campo2, String campo3, String nome,String email, String senha) throws Exception {
+        String retorno = "Nada aconteceu ainda";
+        try {
+        Connection conexao = MySQLConnector.conectar();
+        
+            String strSqlDeleteRegister = "delete from `" + db + "`.`" + tbl + "` where `" + campo + "` = ? and`" + campo2 + "` = ? and`" + campo3 + "` = ?;";
+                PreparedStatement rstDeleteRegister = conexao.prepareStatement(strSqlDeleteRegister);
+                rstDeleteRegister.setString(1, nome);
+                rstDeleteRegister.setString(2, email);
+                rstDeleteRegister.setString(3, senha);
+                int linhaRegistro = rstDeleteRegister.executeUpdate();
+                rstDeleteRegister.close();
+                conexao.close();
+                if (linhaRegistro > 0) {
+                    retorno = "Registro excluido com sucesso";
+                }else {
+                    retorno = "Nenhum registro encontrado";
+                }
+        } catch (Exception e) {
+            retorno = "Ops ocorreu erro" + e;
+            System.out.println(retorno);
+        }
+        return null;
+    }       
+
+
+    public static String updateRegistro(String db, String tbl, String id, String nome, String email, String senha) throws Exception {
+        String retorno = "Oi";
+        try {
+            Connection conexao = MySQLConnector.conectar();
+            String strSqlUltimoRegistro = "update `" + db + "`.`" + tbl + "` set `nome` = '" + nome + "', `email` = '" + email + "', `senha` = '" + senha + "' where `id` = " + id + ";";
+            Statement stmSqlUltimoRegistro = conexao.createStatement();
+            stmSqlUltimoRegistro.addBatch(strSqlUltimoRegistro);
+            stmSqlUltimoRegistro.executeBatch();
+            stmSqlUltimoRegistro.close();
+            return retorno;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public static String editarRegistro(String db, String tbl, String id, String nome, String email, String senha) throws Exception {
+        String retorno = "Oi";
+        try {
+            Connection conexao = MySQLConnector.conectar();
+            String strSqlEditarRegistro = "editar `" + db + "`.`" + tbl + "` set `nome` = '" + nome + "', `email` = '" + email + "', `senha` = '" + senha + "' where `id` = " + id + ";";
+            Statement stmSqlEditarRegistro = conexao.createStatement();
+            stmSqlEditarRegistro.addBatch(strSqlEditarRegistro);
+            stmSqlEditarRegistro.executeBatch();
+            stmSqlEditarRegistro.close();
+            return retorno;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public  static String novoRegistro(String db, String tbl,String campo1,String campo2,String campo3, String nome, String email, String senha) {
+        String retorno = "Nada aconteceu ainda...";
+            try {
+                Connection conexao = MySQLConnector.conectar();
+                String strSqlNovoRegistro = "insert into `" + db +"`.`" + tbl +"` (`" + campo1 + "`, `" + campo2 + "`, `" + campo3 + "`) values ('" + nome + "', '"+ email +"', '" + senha + "');";
+                Statement stmSqlNovoRegistro = conexao.createStatement();
+                stmSqlNovoRegistro.addBatch(strSqlNovoRegistro);
+                stmSqlNovoRegistro.executeBatch();
+                stmSqlNovoRegistro.close();        
+                retorno = ("Novo registro inserido com sucesso !");
+                System.out.println(retorno);
+          } catch (Exception e) {
+            retorno = "Ops! ocorreu o erro:" + e;
+            System.out.println(retorno);
+          }
+         return retorno;
+        }
+        
+        public  static String verRegistro(String db, String tbl,String campo1,String campo2,String campo3, String nome, String email, String senha) {
+            String retorno = "Nada aconteceu ainda...";
+                try {
+                    Connection conexao = MySQLConnector.conectar();
+                    String strSqlVerRegistro = "insert into `" + db +"`.`" + tbl +"` (`" + campo1 + "`, `" + campo2 + "`, `" + campo3 + "`) values ('" + nome + "', '"+ email +"', '" + senha + "');";
+                    Statement stmSqlVerRegistro = conexao.createStatement();
+                    stmSqlVerRegistro.addBatch(strSqlVerRegistro);
+                    stmSqlVerRegistro.executeBatch();
+                    stmSqlVerRegistro.close();        
+                    retorno = ("Novo registro inserido com sucesso !");
+                    System.out.println(retorno);
+              } catch (Exception e) {
+                retorno = "Ops! ocorreu o erro:" + e;
+                System.out.println(retorno);
+              }
+             return retorno;
+            }
+
+    public static String[] anteriorRegistro(String db, String tbl, String id) throws Exception {
+        Connection conexao = MySQLConnector.conectar();
+        int idPessoa = Integer.parseInt(id);
+        int proximoId = idPessoa - 1;
+        if(proximoId >= 1) {
+            String strSqlAnteriorRegistro = "select * from `" + db + "`.`" + tbl + "`order by `id` desc;";
+            Statement stmSqlAnteriorRegistro = conexao.createStatement();
+            ResultSet rstSqlanteriorRegistro = stmSqlAnteriorRegistro.executeQuery(strSqlAnteriorRegistro);
+            String[] resultado = {"","","", ""};
+            while (rstSqlanteriorRegistro.next()) {
+            if (id.equals(rstSqlanteriorRegistro.getString("id"))) {
+                rstSqlanteriorRegistro.next();
+                resultado[0] = rstSqlanteriorRegistro.getString("id");
+                resultado[1] = rstSqlanteriorRegistro.getString("nome");
+                resultado[2] = rstSqlanteriorRegistro.getString("email");
+                resultado[3] = rstSqlanteriorRegistro.getString("senha");
+            break;
+            }
+            }
+            stmSqlAnteriorRegistro.close();
+            if (resultado[0] == ""){
+                return null;
+            } else {
+                return resultado;
+            }
+        } else {
+            return null;
+        }
+    }
+}
